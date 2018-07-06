6f4a9df282c663767a5cc04f1fe053f98e892c26
b2ogeyman <kostyamyagkov@gmail.com>
2018-07-06 04:12:14 -0700

    Added Therapy, Explore and Connect with Friends mock pages.

--- FoodTracker/MainTabBarController.swift
+++ FoodTracker/MainTabBarController.swift
@@ -1,20 +1,19 @@
 //
-//  HomeViewController.swift
+//  MainTabBarController.swift
 //  FoodTracker
 //
-//  Created by kostya on 7/3/18.
+//  Created by kostya on 7/6/18.
 //  Copyright © 2018 kostya. All rights reserved.
 //
 
 import UIKit
-import LGButton
 
-class HomeViewController: UIViewController {
-    
+class MainTabBarController: UITabBarController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        selectedIndex = 1
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,19 +31,5 @@ class HomeViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-    
-    
-    @IBAction func therapyButton(_ sender: LGButton) {
-        self.tabBarController!.selectedIndex = 2   
-    }
-    
-    
-    @IBAction func connectFriendsButton(_ sender: LGButton) {
-        performSegue(withIdentifier: "toFriends", sender: self)
-    }
-    
-    
-    @IBAction func explore(_ sender: LGButton) {
-        performSegue(withIdentifier: "toExplore", sender: self)
-    }
+
 }
